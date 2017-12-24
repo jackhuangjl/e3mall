ea8819c67206a08f0f9a3939f217a89e0a2bc3d1
jackhuangjl <874706726@qq.com>
2017-12-24 17:34:19 +0800

    init e3-portal-web

---
+++ e3-portal-web/src/main/java/cn/e3mall/portal/controller/IndexController.java
@@ -0,0 +1,28 @@
+package cn.e3mall.portal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.e3mall.pojo.TbContent;
+
+/**
+ * 首页展示Controller
+ * <p>Title: IndexController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Controller
+public class IndexController {
+		
+	@RequestMapping("/index")
+	public String showIndex(Model model) {
+
+		return "index";
+	}
+}
